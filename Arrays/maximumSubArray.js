33769b499f444fd0a58950a80e4a2ef5ed9df87a
rafeulanamudoy <rafeuludoy@gmail.com>
2026-03-08 15:10:34 +0600

    maximum subArray problem solved by brute force approach

---
+++ Arrays/maximumSubArray.js
@@ -0,0 +1,18 @@
+var maxSubArray = function (nums) {
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (j = i; j < nums.length; j++) {
+      sum = sum + nums[j];
+
+      maxSum = Math.max(maxSum, sum);
+    }
+    sum=0
+  }
+  return maxSum
+};
+
+const result = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+
+console.log(result)
