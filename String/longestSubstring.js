caab3acaa04bbca0750f7c39cc48d1e5bc74bd1b
rafeulanamudoy <rafeuludoy@gmail.com>
2026-05-09 11:54:00 +0600

    sliding window technique used in longest substring

--- String/longestSubstring.js
+++ String/longestSubstring.js
@@ -1,55 +1,90 @@
+//brute force approach time complexity o n3 
+// var lengthOfLongestSubstring = function (s) {
 
+
+
+
+//     if (!s.length) {
+//         return 0
+//     }
+
+
+//     let globalLongestSubstring = s[0].toString()
+//     for (let i = 0; i < s.length - 1; i++) {
+
+
+//         let localLongestSubstring = s[i].toString()
+//         for (let j = i + 1; j <= s.length - 1; j++) {
+
+
+//             if (localLongestSubstring.includes(s[j])) {
+
+
+//                 break
+//             }
+
+
+//             localLongestSubstring = localLongestSubstring.concat(s[j])
+//             console.log(localLongestSubstring)
+
+
+//         }
+
+
+//         if (localLongestSubstring.length > globalLongestSubstring.length) {
+
+//             globalLongestSubstring = localLongestSubstring
+
+//         }
+
+
+//     }
+//     return globalLongestSubstring.length
+
+// };
+//sliding window techniqu
 var lengthOfLongestSubstring = function (s) {
 
+    // let maxLength = 0;
+    // let trackString = new Map()
 
-    // console.log(s.length,"check")
 
-    if (!s.length) {
-        return 0
-    }
+    // let left = 0
 
-    // if (s.length && s.trim() === "") {
-    //     return 1
+    // for (let right = 0; right < s.length; right++) {
+
+    //     if (trackString.has(s[right])) {
+
+    //         left = left + 1;
+       
+    //         continue
+    //     }
+
+    //     trackString.set(s[right], right)
+    
+    //     maxLength = Math.max(maxLength, trackString.size)
+
+
     // }
-    let globalLongestSubstring = s[0].toString()
+    // return maxLength
+    let map = new Map();  // char → last seen index
+    let left = 0;
+    let maxLength = 0;
 
+    for (let right = 0; right < s.length; right++) {
 
-
-
-
-    for (let i = 0; i < s.length - 1; i++) {
-        // console.log(i,"cehck ")
-
-        let localLongestSubstring = s[i].toString()
-        for (let j = i + 1; j <= s.length - 1; j++) {
-
-           console.log(j,"nested loop")
-            if (localLongestSubstring.includes(s[j])) {
-
-                // console.log(s[i],s[j],"in loop of nester")
-                break
-            }
-
-
-            localLongestSubstring = localLongestSubstring.concat(s[j])
-            console.log(localLongestSubstring)
-
-
+        if (map.has(s[right]) && map.get(s[right]) >= left) {
+            left = map.get(s[right]) + 1; 
         }
 
-
-        if (localLongestSubstring.length > globalLongestSubstring.length) {
-
-            globalLongestSubstring = localLongestSubstring
-
-        }
-
-
+        map.set(s[right], right);  
+        console.log(map,right)
+        maxLength = Math.max(maxLength, right - left + 1);
     }
-    return globalLongestSubstring.length
+
+    return maxLength;
 
 };
 
-
-const result = lengthOfLongestSubstring("au")
+const result = lengthOfLongestSubstring("pwwkew")
 console.log(result)
